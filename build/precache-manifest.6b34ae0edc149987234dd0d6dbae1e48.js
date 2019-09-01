8bd06570f12d422d9fdc57faa90c22d44c8e6156
Faby <oluwafemiegbodofo#gmail.com>
2019-09-01 13:49:28 +0100

    Modified app.js and .gitignore

---
+++ build/precache-manifest.6b34ae0edc149987234dd0d6dbae1e48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a273cf2b07c0aa893c8677d70a0aaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a278c818cd7cb18120f4",
+    "url": "/static/css/main.3abd35e9.chunk.css"
+  },
+  {
+    "revision": "d5616b1c28c53d320877",
+    "url": "/static/js/2.10216792.chunk.js"
+  },
+  {
+    "revision": "5e1cccfbf32b9048a5aa",
+    "url": "/static/js/3.17e427bd.chunk.js"
+  },
+  {
+    "revision": "499b056dfbe7f29fe475",
+    "url": "/static/js/4.c1b12d96.chunk.js"
+  },
+  {
+    "revision": "fd83b964a9b5c17cc662",
+    "url": "/static/js/5.786e0ccf.chunk.js"
+  },
+  {
+    "revision": "2d6494892173395ff364",
+    "url": "/static/js/6.fe1c168d.chunk.js"
+  },
+  {
+    "revision": "a278c818cd7cb18120f4",
+    "url": "/static/js/main.a781e953.chunk.js"
+  },
+  {
+    "revision": "76b663d0d48cad7c009d",
+    "url": "/static/js/runtime~main.caae30f7.js"
+  }
+]);
